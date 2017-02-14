bf00b290c897ccd53a05c2be4d8cf5d191e2b9e3
hkon123 <hkonhd@gmail.com>
2017-02-14 17:57:00 +0000

    Final changes
    It's done

---
+++ complex1.py
@@ -0,0 +1,30 @@
+import math
+
+
+class complex2(object):
+
+    def __init__(self,real,comp):
+        self.real = real
+        self.comp = comp
+
+    def __add__(self,other):
+        a = complex(0,0)
+        a.real = self.real + other.real
+        a.comp = self.comp + other.comp
+        return a
+
+    def __sub__(self,other):
+        a = complex(0,0)
+        a.real = self.real - other.real
+        a.comp = self.comp - other.comp
+        return a
+
+    def __mul__(self,other):
+        a = complex(0,0)
+        a.real = self.real*other.real - self.comp*other.comp
+        a.comp = self.real*other.comp + self.comp*other.real
+        return a
+
+    def length(self):
+        s = math.sqrt(self.real**2 + self.comp**2)
+        return s
--- mandelbrot.py
+++ mandelbrot.py
@@ -1,14 +1,43 @@
 import math
-import Mathplotlib.pyplot as plt
+import matplotlib.pyplot as plt
 import numpy as np
-import complex
 import decimal
 
+
+class complex1(object):
+
+    def __init__(self,real,comp):
+        self.real = real
+        self.comp = comp
+
+    def __add__(self,other):
+        a = complex1(0,0)
+        a.real = self.real + other.real
+        a.comp = self.comp + other.comp
+        return a
+
+    def __sub__(self,other):
+        a = complex1(0,0)
+        a.real = self.real - other.real
+        a.comp = self.comp - other.comp
+        return a
+
+    def __mul__(self,other):
+        a = complex1(0,0)
+        a.real = self.real*other.real - self.comp*other.comp
+        a.comp = self.real*other.comp + self.comp*other.real
+        return a
+
+    def length(self):
+        s = math.sqrt(self.real**2 + self.comp**2)
+        return float(s)
+
+
 def mandel(c):
-    z = complex(0,0)
-    zn = complex(0,0)
+    z = complex1(0,0)
+    zn = complex1(0,0)
     count = 0
-    while zn.length**2 <=4 and count <255:
+    while zn.length()**2 <=4 and count <255:
          zn = z*z+c
          z = zn
          count = count + 1
@@ -22,23 +51,35 @@ class mandelbrot(object):
     def __init__(self,step):
         self.x = np.arange(-2.025,0.6,step)
         self.y = np.arange(-1.125,1.125,step)
-        self.comped = np.zeros((len(self.x),len(self.y)))
+        self.comped = np.zeros((len(self.y),len(self.x)))
 
     def compute(self):
         for i in range(0,len(self.x)):
             for j in range(0,len(self.y)):
-                self.comped[j,i]=mandel(complex(self.x[i],self.y[j]))
+                self.comped[j,i]=mandel(complex1(self.x[i],self.y[j]))
 
     def mPrint(self):
-        left = self.x.min()
-        right = len(self.x)
-        bottom = len(self.y)
-        top = self.y.min()
-        plt.imshow(self.comped, extent=(left,right,bottom, top), interpolation = 'none', cmap='hot')
+        left = self.x[0]
+        right = self.x[-1]
+        bottom = self.y[-1]
+        top = self.y[0]
+        plt.imshow(self.comped, extent=(left,right,bottom, top), cmap='hot')
         plt.colorbar()
+        plt.title("mandelbrot set for c{-2.025-1.125i-->0.6+1.125i}")
+        plt.xlabel("real part")
+        plt.ylabel("complex part")
         plt.show()
 
 def main():
-    sett = mandelbrot(0.1)
+    while True:
+        try:
+            step = float(raw_input("how big a step between the points "))
+            break
+        except ValueError:
+            print("use a small number, jeez")
+    
+    sett = mandelbrot(step)
     sett.compute()
     sett.mPrint()
+
+main()
